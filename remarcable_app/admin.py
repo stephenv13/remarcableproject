7528071664a0a08a7cbd9f3d470dd078585f7f07
stephenv13 <stephenvogelmeier@gmail.com>
2022-08-23 20:50:12 -0400

    Added models

--- remarcable_app/admin.py
+++ remarcable_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from remarcable_app.models import Product, Category, Tag, TagProductRelationship
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(TagProductRelationship)
--- remarcable_app/models.py
+++ remarcable_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+"""
+This model class will produce a table containing a pk column, product_name column, and a category fk column. The fk links each
+product with its corresponding category.
+"""
+class Product(models.Model):
+    product_name = models.CharField(max_length=20)
+    category = models.ForeignKey('remarcable_app.category', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.product_name
+
+
+"""
+This model class will produce a table containing a pk column and a category_name column.
+"""
+class Category(models.Model):
+    category_name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.category_name
+
+
+"""
+This model class will produce a table containing a pk column and a tag_name column.
+"""
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.tag_name
+
+"""
+This model class will produce a table containing a pk column and two fk columns. These fk columns link each product with
+its corresponding tags.
+"""
+class TagProductRelationship(models.Model):
+    product = models.ForeignKey("remarcable_app.product",on_delete=models.CASCADE)
+    tag = models.ForeignKey("remarcable_app.tag",on_delete=models.CASCADE)
+    
